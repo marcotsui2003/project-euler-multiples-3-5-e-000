e05ac22ac585bdf444aca383d094074cd6dbddc4
marcotsui2003 <marcotsui2003@yahoo.com>
2015-10-27 21:42:12 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
-# Enter your procedural solution here!
+def collect_multiples(number = 100)
+  number = number.to_i
+  multiples_array =[]
+  for i in 1..number-1
+    if i % 3 == 0 || i % 5 == 0
+      multiples_array.push(i)
+    end
+  end
+  return multiples_array
+end
+
+def sum_multiples(limit = 100)
+  multiples_array = collect_multiples (limit)
+  sum = 0
+  multiples_array.each do |i|
+    sum += i
+  end
+  sum
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def limit= (number)
+    @limit = number
+  end
+
+  def collect_multiples
+    @limit = @limit.to_i
+    multiples_array =[]
+    for i in 1..@limit - 1
+      if i % 3 == 0 || i % 5 == 0
+        multiples_array.push(i)
+      end
+    end
+    return multiples_array
+  end
+
+  def sum_multiples
+    multiples_array = collect_multiples
+    sum = 0
+    multiples_array.each do |i|
+      sum += i
+    end
+    sum
+  end
+end
+
